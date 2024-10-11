3d275460ba8e64a68908bc980b717dc345da7a0f
donnyfauzi <donnyfauzi55@gmail.com>
2024-10-11 16:57:34 +0700

    update fiture

--- src/main/java/com/example/myApp/homeCotroller.java
+++ src/main/java/com/example/myApp/homeCotroller.java
@@ -1,15 +1,14 @@
 package com.example.myApp;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class homeCotroller {
 
      @GetMapping("/")
-     public String home(Model model) {
-         model.addAttribute("message");
+     public String home() {
          return "home"; // Mengembalikan nama template (home.html)
      }
+
 }
