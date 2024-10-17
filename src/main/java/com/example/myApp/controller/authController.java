275129e758aed4b312f675e34dd7e05c8b949bf1
donnyfauzi <donnyfauzi55@gmail.com>
2024-10-17 12:35:33 +0700

    update fitur

--- src/main/java/com/example/myApp/controller/authController.java
+++ src/main/java/com/example/myApp/controller/authController.java
@@ -2,19 +2,24 @@ package com.example.myApp.controller;
 
 import com.example.myApp.model.User;
 import com.example.myApp.service.UserService;
-
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AuthController {
+
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
 
     @GetMapping("/login")
     public String login() {
@@ -29,17 +34,19 @@ public class AuthController {
         System.out.println("Password Input: " + password);
         
         if (user.isPresent()) {
+            // Menggunakan passwordEncoder.matches untuk memverifikasi password
+            boolean passwordMatch = passwordEncoder.matches(password, user.get().getPassword());
             System.out.println("User Found: " + user.get().getEmail() + ", Password: " + user.get().getPassword());
+
+            if (passwordMatch) {
+                // Jika login berhasil, arahkan ke halaman beranda atau dashboard
+                return "redirect:/dashboard";
+            }
         }
         
-        if (user.isPresent() && user.get().getPassword().equals(password)) {
-            // Jika login berhasil, arahkan ke halaman beranda atau dashboard
-            return "redirect:/dashboard";
-        } else {
-            // Jika login gagal, kembali ke halaman login dengan pesan error
-            model.addAttribute("error", "Invalid email or password");
-            return "login";
-        }
+        // Jika login gagal, kembali ke halaman login dengan pesan error
+        model.addAttribute("error", "Invalid email or password");
+        return "login";
     }
     
     
