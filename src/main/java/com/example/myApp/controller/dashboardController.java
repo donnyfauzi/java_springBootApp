d5b44be10fd29aeaf527c75ad455aceb1c244c33
donnyfauzi <donnyfauzi55@gmail.com>
2024-12-07 16:15:51 +0700

    update fiture

--- src/main/java/com/example/myApp/controller/dashboardController.java
+++ src/main/java/com/example/myApp/controller/dashboardController.java
@@ -9,25 +9,13 @@ public class DashboardController {
 
     @GetMapping("/dashboard")
     public String showDashboard(Model model) {
-        model.addAttribute("menu", "dashboard");
-        model.addAttribute("pageTitle", "Dashboard");
-        model.addAttribute("content", ""); // Mengarahkan konten dashboard
         return "users-profile"; // Menggunakan layout utama dashboard
     }
 
     @GetMapping("/data-pribadi")
     public String showPengguna(Model model) {
-        model.addAttribute("menu", "data-pribadi");
-        model.addAttribute("pageTitle", "data-pribadi");
-        model.addAttribute("content", "data-pribadi.html"); // Mengarahkan konten pengguna
-        return "layout-dashboard"; // Menggunakan layout utama dashboard
+        return "forms-validation"; // Menggunakan layout utama dashboard
     }
 
-    @GetMapping("/pendidikan")
-    public String showProduk(Model model) {
-        model.addAttribute("menu", "pendidikan");
-        model.addAttribute("pageTitle", "pendidikan");
-        model.addAttribute("content", "pendidikan.html"); // Mengarahkan konten produk
-        return "layout-dashboard"; // Menggunakan layout utama dashboard
-    }
+   
 }
