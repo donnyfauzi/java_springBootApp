4aea95c570b52b68741cf95d64a54e00152bb264
donnyfauzi <donnyfauzi55@gmail.com>
2024-12-10 11:47:46 +0700

    update fiture

--- src/main/java/com/example/myApp/controller/DataPribadiController.java
+++ src/main/java/com/example/myApp/controller/DataPribadiController.java
@@ -5,14 +5,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class DashboardController {
-
-    @GetMapping("/dashboard")
-    public String showDashboard(Model model) {
-        return "users-profile"; // Menggunakan layout utama dashboard
+public class DataPribadiController {
+    @GetMapping("/data-pribadi")
+    public String showPengguna(Model model) {
+        return "data-pribadi"; 
     }
-
-    
-
-   
 }
