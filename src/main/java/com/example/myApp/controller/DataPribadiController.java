534e56cfb59df8cd5a9028f89dc59736566dcfff
donnyfauzi <donnyfauzi55@gmail.com>
2024-12-28 12:32:08 +0700

    update

--- src/main/java/com/example/myApp/controller/DataPribadiController.java
+++ src/main/java/com/example/myApp/controller/DataPribadiController.java
@@ -43,11 +43,14 @@ public class DataPribadiController {
             RedirectAttributes redirectAttributes,
             Model model
     ) {
+        model.addAttribute("nama", nama);
         //validasi input
         if (nama == null || nama.trim().isEmpty()) {
             model.addAttribute("errorNama", "Nama tidak boleh kosong !");
             return "data-pribadi"; // Kembali ke halaman dengan pesan error
         }
+
+        System.out.println("Nama: " + nama);
 
         if (jenisKelamin == null || jenisKelamin.trim().isEmpty()) {
             model.addAttribute("errorJenisKelamin", "Jenis Kelamin harus diisi !");
@@ -55,9 +58,57 @@ public class DataPribadiController {
         }
 
         if (tentangAnda == null || tentangAnda.trim().isEmpty()) {
-            model.addAttribute("errorTentangAnda", "Kolom ini harus diisi !");
+            model.addAttribute("errorTentangAnda", "Tentang Anda harus diisi !");
             return "data-pribadi";
         }
+
+        if (alamat == null || alamat.trim().isEmpty()) {
+            model.addAttribute("errorAlamat", "Alamat harus diisi !");
+            return "data-pribadi";
+        }
+
+        if (tempatLahir == null || tempatLahir.trim().isEmpty()) {
+            model.addAttribute("errorTempatLahir", "Tempat Lahir harus diisi !");
+            return "data-pribadi";
+        }
+
+        if (tanggalLahir == null || tanggalLahir.trim().isEmpty()) {
+            model.addAttribute("errorTanggalLahir", "Tanggal Lahir harus diisi !");
+            return "data-pribadi";
+        }
+
+        if (noTlp == null || noTlp.trim().isEmpty()) {
+            model.addAttribute("errorNoTlp", "Nomor Tlp harus diisi !");
+            return "data-pribadi";
+        }
+
+        if (email == null || email.trim().isEmpty()) {
+            model.addAttribute("errorEmail", "Email harus diisi !");
+            return "data-pribadi";
+        }
+
+        if (agama == null || agama.trim().isEmpty()) {
+            model.addAttribute("errorAgama", "Agama harus diisi !");
+            return "data-pribadi";
+        }
+
+        if (statusPerkawinan == null || statusPerkawinan.trim().isEmpty()) {
+            model.addAttribute("errorStatusPerkawinan", "Status Perkawinan harus diisi !");
+            return "data-pribadi";
+        }
+
+        if (golDarah == null || golDarah.trim().isEmpty()) {
+            model.addAttribute("errorGolDarah", "Golongan Darah harus diisi !");
+            return "data-pribadi";
+        }
+
+        if (kewarganegaraan == null || kewarganegaraan.trim().isEmpty()) {
+            model.addAttribute("errorKewarganegaraan", "Kewarganegaraan harus diisi !");
+            return "data-pribadi";
+        }
+
+        
+
 
         // Membuat objek DataPribadi dan mengisinya dengan data dari form
         DataPribadi dataPribadi = new DataPribadi();
